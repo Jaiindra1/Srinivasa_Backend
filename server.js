44d15014854ce4a2efe590155cf9276a2a36bba0
Jai indra <168549352+Jaiindra1@users.noreply.github.com>
2025-02-26 21:58:18 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const express = require("express");
 const path = require("path");
 const cors = require("cors"); // Import CORS package
-
 const app = express();
 app.use(express.json());
 app.use(cors()); // Enable CORS for all requests
-
-
-
-    app.listen(5000, () => {
+app.listen(5000, () => {
       console.log("Server Running at http://localhost:5000/");
-    });
+});
  
 
 
